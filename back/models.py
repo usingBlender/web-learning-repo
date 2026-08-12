2f74ef26f51625073dfafc59fb3e0420d1617d5d
usingBlender <enzomedeirossena@gmail.com>
2026-08-12 13:21:06 -0300

    alembic & models

--- back/models.py
+++ back/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
 from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils import ChoiceType
 
 db = create_engine("sqlite:///banco.db")
 
@@ -22,3 +23,39 @@ class Usuario(Base):
         self.ativo = ativo
         self.admin = admin
         # 22:30 (03)
+
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    
+    STATUS_PEDIDOS = (
+            ("PENDENTE", "PENDENTE"),
+            ("CANCELADO", "CANCELADO"),
+            ("FINALIZADO", "FINALIZADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS))
+    usuario = Column("usuario", ForeignKey("usuario.id"))
+    preco = Column("preco", Float)
+
+    def __init__(self, usuario, status="PENDENTE", preco=0):
+        self.usuario = usuario
+        self.preco = preco
+        self.status = status
+
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantidade = Column("quantidade", Integer)
+    sabor = Column("sabor", Integer)
+    tamanho = Column("tamanho", String)
+    preco_unitario = Column("preco_unitario", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+    def __init__(self, quantidade, sabor, tamanho, preco_unitario, pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho = tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
